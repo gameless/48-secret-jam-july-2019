2cf47bf3790a743c42009ef9e716840b35303d99
Brandon Canfield <sneelhorses@gmail.com>
2019-07-19 22:01:04 -0400

    Webserverify

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+
+const path = require('path');
+
+const PORT = process.env.PORT || 4000;
+
+app.use('/', express.static(path.join(__dirname, 'static')));
+
+app.get('/', function(req, res) {
+	res.sendFile(path.join(__dirname, 'static/index.html'));
+});
+
+app.listen(PORT, () => {
+	console.log('Okay, this is epic');
+	console.log('Listening on port ' + PORT);
+});
