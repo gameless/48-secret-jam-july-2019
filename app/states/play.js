5f4a18944260f1e2b86b802e5b22878371d55de8
Sam Estep <sam@samestep.com>
2019-07-20 16:37:19 -0400

    Draw a trealpezoid

--- app/states/play.js
+++ app/states/play.js
@@ -2,5 +2,15 @@ export default class extends Phaser.State {
   create() {
     this.stage.backgroundColor = '#ffffff';
     this.game.add.image(0, 0, 'insect');
+
+    const graphics = this.game.add.graphics(50, 50);
+    graphics.beginFill(0x00ffff);
+    graphics.drawPolygon([
+      new Phaser.Point(50, 50),
+      new Phaser.Point(50, 60),
+      new Phaser.Point(60, 60),
+      new Phaser.Point(55, 50),
+    ]);
+    graphics.endFill();
   }
 }
